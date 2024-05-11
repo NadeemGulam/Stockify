a85ac27eca24801a445c4442a9c2549845d4f275
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-05-11 14:45:06 +0530

    trest

--- app/api/mongo/route.js
+++ app/api/mongo/route.js
@@ -1,21 +1,21 @@
-// import { MongoClient } from "mongodb";
-// import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
 
-// export async function GET(request) {
-// // Replace the uri string with your connection string.
-// const uri = "mongodb+srv://Nadeem:DmEbPuKMK6AkyCGj@cluster0.u1dvzec.mongodb.net/";
-// const client = new MongoClient(uri); 
-//   try {
-//     const database = client.db('test');
-//     const movies = database.collection('inventory');
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = {  };
-//     const movie = await movies.find(query).toArray();
-//     console.log(movie);
-//     return NextResponse.json({"a": 34, movie})
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   } 
+export async function GET(request) {
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://Nadeem:DmEbPuKMK6AkyCGj@cluster0.u1dvzec.mongodb.net/";
+const client = new MongoClient(uri); 
+  try {
+    const database = client.db('test');
+    const movies = database.collection('inventory');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = {  };
+    const movie = await movies.find(query).toArray();
+    console.log(movie);
+    return NextResponse.json({"a": 34, movie})
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  } 
 
-// }
+}
